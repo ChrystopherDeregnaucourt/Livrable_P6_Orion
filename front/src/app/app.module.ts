417f0fa468f668cb4bbaa06287bd6abc31d67232
DESKTOP-RADQGCI\dereg <deregnaucourt.chrystopher@hotmail.fr>
2025-11-11 07:15:24 +0100

    créage pages

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopicComponent } from './topic/topic.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopicComponent
-  ],
+  declarations: [AppComponent, HomeComponent, RegisterComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
