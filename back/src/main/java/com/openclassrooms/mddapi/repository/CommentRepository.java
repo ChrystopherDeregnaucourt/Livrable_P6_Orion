904c896f2f2d8e9900483b440a32d595d20571aa
Chrys <deregnaucourt.chrystopher@hotmail.fr>
2025-11-19 09:49:10 +0100

    MAJ des repos

--- back/src/main/java/com/openclassrooms/mddapi/repository/CommentRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/CommentRepository.java
@@ -1,11 +1,12 @@
 package com.openclassrooms.mddapi.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.openclassrooms.mddapi.model.Comment;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    // Récupérer les commentaires d'un article, souvent triés par date
+    List<Comment> findByPostId(Long postId);
 }
--- back/src/main/java/com/openclassrooms/mddapi/repository/PostRepository.java
+++ back/src/main/java/com/openclassrooms/mddapi/repository/PostRepository.java
@@ -1,11 +1,12 @@
 package com.openclassrooms.mddapi.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.openclassrooms.mddapi.model.Post;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long>{
-
+public interface PostRepository extends JpaRepository<Post, Long> {
+    // Pour trouver tous les articles d'un thème spécifique
+    List<Post> findByTopicId(Long topicId);
 }
